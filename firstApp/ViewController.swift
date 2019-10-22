0fbf30194d264f8b312f1c7e04cd46c80296632a
Jonh <jonhgx@gmail.com>
2019-10-22 11:39:37 -0500

    primer app mega basica con logo integrado

--- firstApp/ViewController.swift
+++ firstApp/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var texto: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Cargada!")
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func boton(_ sender: UIButton) {
+        self.texto.resignFirstResponder()
+        let alerta = UIAlertController(title: "Bienvenido", message: texto.text, preferredStyle: .alert)
+        let accion = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        
+        alerta.addAction(accion)
+        present(alerta, animated: true, completion: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
